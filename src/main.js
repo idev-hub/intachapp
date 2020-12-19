9a753c4601d3bbd62dcfa0476c6acd657350dba2
idev-hub <7vg7tek7sG>
2020-12-19 15:04:04 +0500

    init

--- src/main.js
+++ src/main.js
@@ -9,10 +9,4 @@ import bridge from '@vkontakte/vk-bridge'
 const Vue = createApp(App)
 
 bridge.send("VKWebAppInit", {}).catch(console.error)
-bridge.send('VKWebAppGetEmail')
-    .then(data => {
-        console.log(data.email)
-    })
-    .catch(console.error)
-
 Vue.use(store).use(router).use(VueAxios, axios).mount('#app')
